e98e5a59f761b9f7eae96171c107a73d517382b0
kpochodyla <kpochodyla@protonmail.com>
2023-03-20 20:41:07 +0100

    Add connection to influxdb

--- locust/settings/settings.py
+++ locust/settings/settings.py
@@ -12,35 +12,35 @@ class TestSettings(BaseSettings):
             "last_name": "Bluth",
             "avatar": "https://reqres.in/img/faces/1-image.jpg",
         },
-        "user2":{
+        "user2": {
             "id": 2,
             "email": "janet.weaver@reqres.in",
             "first_name": "Janet",
             "last_name": "Weaver",
             "avatar": "https://reqres.in/img/faces/2-image.jpg",
         },
-        "user3":{
+        "user3": {
             "id": 3,
             "email": "emma.wong@reqres.in",
             "first_name": "Emma",
             "last_name": "Wong",
             "avatar": "https://reqres.in/img/faces/3-image.jpg",
         },
-        "user4":{
+        "user4": {
             "id": 4,
             "email": "eve.holt@reqres.in",
             "first_name": "Eve",
             "last_name": "Holt",
             "avatar": "https://reqres.in/img/faces/4-image.jpg",
         },
-        "user5":{
+        "user5": {
             "id": 5,
             "email": "charles.morris@reqres.in",
             "first_name": "Charles",
             "last_name": "Morris",
             "avatar": "https://reqres.in/img/faces/5-image.jpg",
         },
-        "user6":{
+        "user6": {
             "id": 6,
             "email": "tracey.ramos@reqres.in",
             "first_name": "Tracey",
--- locust/tests/test_get_list_of_users.py
+++ locust/tests/test_get_list_of_users.py
@@ -1,11 +1,23 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, events
 from settings.settings import TestSettings
+from influxdb import InfluxDBClient
+import json
+import datetime
+import pytz
+import socket
+from settings.secrets import secretes
+
+hostname = socket.gethostname()
+client = InfluxDBClient(host=secretes.influxdb_hostname, port=secretes.influxdb_port)
+client.switch_database("locust_data")
+
 
 class LoadUser(HttpUser):
     wait_time = between(0, 5)
     settings = TestSettings()
     users = settings.base_users
     properties = settings.properties
+
     @task
     def hello_world(self):
         response = self.client.get("/api/users")
@@ -13,4 +25,101 @@ class LoadUser(HttpUser):
         assert response.headers["Content-Type"] == "application/json; charset=utf-8"
         data = response.json()["data"]
         assert all(prop in user for user in data for prop in self.properties)
-        assert all(actualUser == expectedUser for expectedUser in self.users.values() for actualUser in data if actualUser["id"] == expectedUser["id"])
+        assert all(
+            actualUser == expectedUser
+            for expectedUser in self.users.values()
+            for actualUser in data
+            if actualUser["id"] == expectedUser["id"]
+        )
+
+    @events.request.add_listener
+    def my_request_handler(
+        request_type,
+        name,
+        response_time,
+        response_length,
+        response,
+        context,
+        exception,
+        start_time,
+        url,
+        **kwargs
+    ):
+        SUCCESS_TEMPLATE = (
+            '[{"measurement": "%s","tags": {"hostname":"%s","requestName": "%s","requestType": "%s","status":"%s"'
+            '},"time":"%s","fields": {"responseTime": "%s","responseLength":"%s"}'
+            "}]"
+        )
+
+        FAIL_TEMPLATE = (
+            '[{"measurement": "%s","tags": {"hostname":"%s","requestName": "%s","requestType": "%s","exception":"%s","status":"%s"'
+            '},"time":"%s","fields": {"responseTime": "%s","responseLength":"%s"}'
+            "}]"
+        )
+
+        if exception:
+            json_string = FAIL_TEMPLATE % (
+                "ResponseTable",
+                hostname,
+                name,
+                request_type,
+                exception,
+                "fail",
+                datetime.datetime.now(tz=pytz.UTC),
+                response_time,
+                response_length,
+            )
+        else:
+            json_string = SUCCESS_TEMPLATE % (
+                "ResponseTable",
+                hostname,
+                name,
+                request_type,
+                "success",
+                datetime.datetime.now(tz=pytz.UTC),
+                response_time,
+                response_length,
+            )
+        client.write_points(json.loads(json_string), time_precision="ms")
+
+    @events.test_stop.add_listener
+    def on_test_stop(environment, **kw):
+        SUMMARY_TEMPLATE = """
+        [
+            {
+                "measurement": "%s",
+                "tags": 
+                    {
+                        "hostname":"%s",
+                        "test_name": "%s"
+                    },
+                "time":"%s",
+                "fields": 
+                    {
+                        "fail_ratio": "%s",
+                        "num_requests":"%s",
+                        "num_failures":"%s",
+                        "total_rps":"%s",
+                        "min_response_time":"%s",
+                        "max_response_time":"%s",
+                        "median_response_time":"%s",
+                        "avg_response_time":"%s"
+                    }
+            }
+        ]
+"""
+        json_string = SUMMARY_TEMPLATE % (
+            "SummaryTable",
+            hostname,
+            environment.locustfile,
+            datetime.datetime.now(tz=pytz.UTC),
+            environment.stats.total.fail_ratio,
+            environment.stats.total.num_requests,
+            environment.stats.total.num_failures,
+            environment.stats.total.total_rps,
+            environment.stats.total.min_response_time,
+            environment.stats.total.max_response_time,
+            environment.stats.total.median_response_time,
+            environment.stats.total.avg_response_time,
+        )
+        client.write_points(json.loads(json_string), time_precision="ms")
